571858faa9d03811bb0773d5c2e8e536070728d5
Adam-Salem <adamsalem99@gmail.com>
2025-03-14 16:25:50 -0700

    Add framerate

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import pygame
+import pygame 
 from constants import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -10,10 +12,12 @@ def main():
     running = True
     while (running):
         for event in pygame.event.get():
-            if event.type == python.QUIT:
+            if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, (1,1,1))
         pygame.display.flip()
+        clock.tick(60)
+        dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
